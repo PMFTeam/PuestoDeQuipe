3ea62ea50aaa3d6535e304e900519e7d1cfa4291
Omar <omar.santana@gmail.com>
2011-04-09 19:49:15 -0400

    Cambio de nombre.

--- EJBPuestoDeQuipe/ejbModule/com/pmf/codejam/middleware/SuplidoraQuisqueyanaMiddleware.java
+++ EJBPuestoDeQuipe/ejbModule/com/pmf/codejam/middleware/SuplidoraQuisqueyanaMiddleware.java
@@ -1,12 +1,12 @@
 package com.pmf.codejam.middleware;
 
-public class SupplierMiddleware {
+public class SuplidoraQuisqueyanaMiddleware {
     /**
 	 * Sends the inventory request via web service to the supplier
 	 */
 	public String sendRequest(InventoryRequest inventoryRequest){
 		//TODO Whole implementation of web service
-		return "Dummy Text";
+		return "OK";
 	}	
 
 }

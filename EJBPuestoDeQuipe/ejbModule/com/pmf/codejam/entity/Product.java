63d9c01695f8e4b3afdd33e398aaed00d25db83d
fpimentel <f.pimentelcruz@gmail.com>
2011-04-09 02:06:39 -0430

    ProductException

--- EJBPuestoDeQuipe/ejbModule/com/pmf/codejam/entity/Product.java
+++ EJBPuestoDeQuipe/ejbModule/com/pmf/codejam/entity/Product.java
@@ -1,5 +1,7 @@
 package com.pmf.codejam.entity;
 import java.io.Serializable;
+
+import com.pmf.codejam.exception.ProductException;
 import com.pmf.codejam.util.EjbConstants;
 import javax.persistence.*;
 import java.util.*;
@@ -59,9 +61,7 @@ public class Product implements Serializable{
 	public void addIngredient(Ingredient ingredient){
 		ingredients.add(ingredient);
 	}	
-	public void restock(int amountProductToRequest){
-		try
-		{
+	public void restock(int amountProductToRequest) throws ProductException{			
 			Set<Ingredient> i = new HashSet<Ingredient>();
 			 for (Iterator<Ingredient> ing = i.iterator(); ing.hasNext(); ){
 				 Ingredient currentIngre = ing.next();
@@ -71,37 +71,25 @@ public class Product implements Serializable{
 			     String unit = currentIngre.getInventoryItem().getUnit();  
 			     int amountIngredientToRequest = amountProductToRequest *  amountIngredientNeeded;
 			     
-			     currentIngre.getInventoryItem().restockingItemQuantity(ingredientId, unit, amountIngredientToRequest);
-			}			
-		}
-		catch(Exception ex){
-			//Do anything..
-		}
-
+			     currentIngre.getInventoryItem().restockingItemQuantity(ingredientId, unit, amountIngredientToRequest);							
+			 }		
 	}
 	/***
 	 * Its Calculate the amount of products that can be done according to what exists in Stock.
 	 * @return
 	 */
-	public int avariableQuantity(){
+	public int avariableQuantity() throws ProductException{
 		int avariableQuantity = 0;
-		try
-		{		
-			Set<Ingredient> i = new HashSet<Ingredient>();
-			 for (Iterator<Ingredient> ing = i.iterator(); ing.hasNext(); ){
-				 int quantityTemp;
-				 Ingredient currentIngre = ing.next();
-				 quantityTemp = currentIngre.getInventoryItem().getQuantity() / currentIngre.getQuantity();
-				 
-				 if(avariableQuantity > quantityTemp)
-					 avariableQuantity = quantityTemp;
-			 }
-	
-		}
-		catch(Exception ex)
-		{
-			//do anything
-		}
+		
+		Set<Ingredient> i = new HashSet<Ingredient>();
+		 for (Iterator<Ingredient> ing = i.iterator(); ing.hasNext(); ){
+			 int quantityTemp;
+			 Ingredient currentIngre = ing.next();
+			 quantityTemp = currentIngre.getInventoryItem().getQuantity() / currentIngre.getQuantity();
+			 
+			 if(avariableQuantity > quantityTemp)
+				 avariableQuantity = quantityTemp;
+		 }
 		return avariableQuantity;		 
 	}
 	public void removeIngredient(Ingredient ingredient){

fa744cc57a8338bdd6f638100cd97a6feba81c06
fpimentel <f.pimentelcruz@gmail.com>
2011-04-08 21:24:56 -0430

    Adding product table constant.

--- EJBPuestoDeQuipe/ejbModule/com/pmf/codejam/util/EjbConstants.java
+++ EJBPuestoDeQuipe/ejbModule/com/pmf/codejam/util/EjbConstants.java
@@ -4,4 +4,5 @@ public interface EjbConstants {
 	public final String TABLE_INVENTORY   = "Inventory";
 	public final String TABLE_INGREDIENTS = "Ingredients";
 	public final String TABLE_SPECIALS = "Specials";
+	public final String TABLE_PRODUCTS = "Products";
 }
